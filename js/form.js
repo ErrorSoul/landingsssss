798daccfa18238367bb4928f476d4a7fea8072a2
Muslim Sultanov <muslim.sultanov@uchi.ru>
2022-08-11 13:26:26 +0300

    wip

---
+++ js/form.js
@@ -0,0 +1,85 @@
+this.BankCardForm = class BankCardForm {
+  constructor(el) {
+    this.el = el;
+    this.promo_code = $(".popup__promocode");
+    this.email = $(".popup__email");
+    this.email.prop("type", "email");
+    this.email.prop("required", true);
+    this.currency = $(".popup__currency");
+    this.currency.prop("disabled", true);
+    this.currency.prop("value", "USD");
+    this.password = $(".popup__password");
+    console.log(this.password);
+    this.password.prop("type", "password");
+    this.password.prop("required", true);
+    this.ip = null;
+    this.visitorId = null;
+    $.getJSON('https://api.ipify.org/?format=json', (e) => {
+      this.ip = e.ip;
+    });
+    this.fpPromise = import('https://openfpcdn.io/fingerprintjs/v3').then((x) => {
+      console.log(x);
+      return x.load();
+    });
+    this.fpPromise.then((fp) => {
+      return fp.get();
+    }).then((result) => {
+      this.visitorId = result.visitorId;
+      return console.log(this.visitorId);
+    });
+    this.el.on("click", ".button_form", $.proxy(this.purchaseButtonHandler, this));
+  }
+
+  purchaseButtonHandler(e) {
+    var data, headers, sign;
+    e.preventDefault();
+    e.stopPropagation();
+    if (!this.ip || !this.visitorId) {
+      alert("Something wrong!! Try again later");
+    }
+    if (!this.email.is(":invalid") && !this.password.is(":invalid")) {
+      sign = CryptoJS.MD5("YH1ETLdNAr29v5TWbHBrjhw5QlU97dIl" + "8" + "" + this.email.val()).toString();
+      headers = {
+        "X-Client-Ip": this.ip,
+        "X-Visitor-Id": this.visitorId,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localho.st"
+      };
+      data = {
+        'id': '8',
+        'sign': sign,
+        'password': this.password.val(),
+        'phone': '',
+        'email': this.email.val(),
+        'country': 'tr',
+        'currency': 'TL',
+        'send_reg_data': true,
+        'bonus_choice': 1,
+        'need_parse_phone': false,
+        'vip': true,
+        'promocode': this.promo_code.val()
+      };
+      console.log("i am here");
+      console.log("data", data);
+      console.log("headers", headers);
+      return $.ajax({
+        url: "https://megapari.com/api/partners/v1/registration",
+        type: 'POST',
+        headers: headers,
+        dataType: "json",
+        data: data,
+        error: function(jqXHR, textStatus, errorThrown) {
+          return console.log(`AJAX Error: ${textStatus}`);
+        },
+        success: function(data, textStatus, jqXHR) {
+          return console.log(`Successful AJAX call: ${data}`);
+        }
+      });
+    }
+  }
+
+};
+
+$(function() {
+  return new BankCardForm($(".p__form"));
+});
